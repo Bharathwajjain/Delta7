e62ebca2e380f1a44ee46431f33d7f0a937bbcb6
Bharathwajjain <bharathwajjain@gmail.com>
2021-02-25 09:51:43 +0100

    First commit

---
+++ src/main/java/io/testproject/generated/tests/delta7/TestLink.java
@@ -0,0 +1,20 @@
+package io.testproject.generated.tests.delta7;
+
+import testlink.api.java.client.TestLinkAPIClient;
+import testlink.api.java.client.TestLinkAPIException;
+import testlink.api.java.client.TestLinkAPIResults;
+public class TestLink{
+	
+	
+	public static String DEVKEY="58a7453f86c7e93d6cbfbc2916ff85dc";
+	public static String URL="http://hello-pomelo-testlink.westeurope.cloudapp.azure.com/lib/api/xmlrpc/v1/xmlrpc.php";
+	static String testProject				= "Delta 7";
+	static String testPlan					= "Delta 7 testing";
+	static String build					= "Version 1";
+	public static void updateTestLinkResult(String testCase, String exception, String result) throws TestLinkAPIException{
+		TestLinkAPIClient testlinkAPIClient = new TestLinkAPIClient(DEVKEY,URL);
+		testlinkAPIClient.reportTestCaseResult(testProject, testPlan, testCase, build, exception, result);
+		 }
+}
+
+
